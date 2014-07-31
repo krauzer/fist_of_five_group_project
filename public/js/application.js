b10d16f680d6e633f797c079d50b72a8c563fa36
Farheen Malik <farheen.m.malik@gmail.com>
2014-07-31 16:32:12 -0400

    Add skeleton for ticket refresh page

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,23 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+	console.log("works")
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+	setTimeout(refreshTickets, 10000);
+ 
 });
+
+
+function refreshTickets() { 
+	// event.preventDefault();
+	window.location.href = window.location.href; 
+	$.ajax({
+		type: 'get',
+		url: '/tickets',
+		data: $(this).serialize()
+	}).success(function(response){
+		console.log("works")
+		console.log(response)
+	}).fail({
+		console.log("works")
+		console.log(response)
+	})
+}
