ca78a861bbc51e4c5c6f8a0193d07f5e8de79c33
Dylan Krause <dykrause@gmail.com>
2014-07-31 15:48:27 -0400

    Initial Skeleton

---
+++ app/controllers/user.rb
@@ -0,0 +1,33 @@
+get '/user/login' do 
+	#User Login page
+	erb :login 
+end
+
+post '/user/login' do 
+	#Set user from login form 
+	  @email = params[:email]
+  user = User.authenticate(@email, params[:password])
+  if user
+    # successfully authenticated; set up session and redirect
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    # an error occurred, re-render the sign-in form, displaying an error
+    @error = "Invalid email or password."
+    erb :login
+  end
+end
+
+post '/users' do
+	#create new user
+	 # sign-up
+  @user = User.new params[:user]
+  if @user.save
+    # successfully created new account; set up the session and redirect
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    # an error occurred, re-render the sign-up form, displaying errors
+    erb :sign_up
+  end
+end
