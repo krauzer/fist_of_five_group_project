e0ff7fe7a6e62ff22065010bb36d65ce0ecb22c8
mhuerster <mary.huerster@gmail.com>
2014-08-01 14:49:46 -0400

    add ability to select element with consistent id

--- public/js/location.js
+++ public/js/location.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
+	for (i=1; i<=42; i++) {
+		$("ul#work_stations").append("<li id='"+i+"''>"+i+"</li>");
+	};
 	$("#work_area li").click(function(e){
 		console.log(this);
 		$(this).css("background-color","yellow");
 	}); // close li.click
-}); // close document.ready
+}); // close document.read
