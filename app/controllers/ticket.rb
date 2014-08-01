18c0a3528bde0a222fc9bf75f9d759a7684edcff
Farheen Malik <farheen.m.malik@gmail.com>
2014-07-31 22:52:50 -0400

    Resolve button on each ticket works - but not with AJAX - began working on this but did not finish

--- app/controllers/ticket.rb
+++ app/controllers/ticket.rb
@@ -11,13 +11,21 @@ post '/tickets' do
 	erb :'partials/_all_tickets'
 end
 
-get '/tickets/:id' do #route for coaches to view info on single tickets
+# get '/tickets/:id/edit' do #route for coaches to view info on single tickets
+# 	@ticket = Ticket.find(params[:id])
+# end
+
+get '/tickets/:id' do #route for coaches to view info on single ticket perhaps in a modal?
 	@ticket = Ticket.find(params[:id])
 
 end
 
 
 post '/tickets/:id' do #route for coach to mark a ticket as resolved
-
+	@ticket = Ticket.find(params[:id])
+	@ticket.resolved = true
+	@ticket.save
+	# erb :'partials/_all_tickets'
+	redirect '/tickets'
 end
 
--- public/js/application.js
+++ public/js/application.js
@@ -6,9 +6,23 @@ $(document).ready(function() {
 	$(".tickets").hover(function(){
 		setTimeout(refreshTickets, 10000);
 
-		console.log("mouseover works")
+		console.log("hover works")
 
 	});
+
+	// $('form#resolvedButton').submit(function(e){
+	// 	e.preventDefault();
+	// 	console.log("resolved button works")
+	// 	console.log($('form#resolvedButton').attr("value"));
+	// 	$.ajax({
+	// 	type: 'post',
+	// 	url: '/tickets/:id'
+	// 	}).success(function(response){
+	// 		console.log("works");
+	// 	}).fail(function(response){
+	// 		console.log("doesn't work")
+	// 	})
+	// });
 
 	$('#createTicket').on('click', openForm);
 
@@ -49,6 +63,13 @@ function refreshTickets() {
 	}).fail({
 		// console.log("Doest not work")
 	})
+}
+
+function refreshPage(){
+	console.log("refresh page works")
+	window.location.href = window.location.href;  
+
+	
 }
 
 function openForm (event) {
