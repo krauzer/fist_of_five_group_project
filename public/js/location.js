4edf10147a0e4500b2d55052eccc9f64b254fb66
mhuerster <mary.huerster@gmail.com>
2014-08-01 15:14:16 -0400

    location selector is getting closer...

--- public/js/location.js
+++ public/js/location.js
@@ -2,8 +2,16 @@ $(document).ready(function(){
 	for (i=1; i<=42; i++) {
 		$("ul#work_stations").append("<li id='"+i+"''>"+i+"</li>");
 	};
-	$("#work_area li").click(function(e){
-		console.log(this);
-		$(this).css("background-color","yellow");
-	}); // close li.click
+	$("form[name='rooms']").change(function(e){
+		console.log("changed the form value");
+		if ($("form[name='rooms'] option:selected").val() == "workstations"){
+			console.log("selected workstations");
+			$("#work_area").show();
+			$("#work_area li").click(function(e){
+				console.log(this);
+				$(this).css("background-color","yellow");
+			}); // close li.click
+		} // close if statement
+	}); // close form.change
+	$("")
 }); // close document.read
